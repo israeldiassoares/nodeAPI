713d420e54b0e6c25c74d71b2b6d8c4d2b4b0393
Israel Soares <israeldiassoares@gmail.com>
2019-05-08 22:50:56 -0300

    created Node Server

---
+++ server.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const http = require("http");
+const debug = require("debug")("nodestr:server");
+const express = require("express");
+
+const app = express();
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+let route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1"
+  });
+});
+
+app.use("/", route);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+console.warn("Servidor API HTTP OUVINDO EM " + port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCESS":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+
+    case "EADDINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port " + address.port;
+  debug("Listening on " + bind);
+}
