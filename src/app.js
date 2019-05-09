07b35f7d16a929fc037f90e98efe315bb0e6ce38
Israel Soares <israeldiassoares@gmail.com>
2019-05-09 00:12:24 -0300

    create crud router

---
+++ src/app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1"
+  });
+});
+
+const create = router.post("/", (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put("/:id", (req, res, next) => {
+  let id = req.params.id;
+  res.status(201).send({
+    id: id,
+    item: req.body
+  });
+});
+
+const deletar = router.delete("/", (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
+app.use("/", route);
+app.use("/products", create);
+app.use("/products", put);
+app.use("/products", deletar);
+
+module.exports = app;
